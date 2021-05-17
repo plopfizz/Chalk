5e28c5e6717e7ea01c212a874f56cba03fb8ee30
Mohit Mandlecha <mohitmandlecha7256@gmail.com>
2021-05-18 00:38:42 +0530

    second commit

--- client/src/components/options/Options.js
+++ client/src/components/options/Options.js
@@ -101,7 +101,7 @@ const Options = () => {
 
           <div className={classes.share_social}>
             <WhatsappShareButton
-              url={`https://video-chat-mihir.web.app/`}
+              url={`https://naughty-shockley-2d2e6e.netlify.app/`}
               title={`Join this meeting with the given code : "${me}"\n`}
               separator="Link: "
               className={classes.share_icon}
@@ -109,14 +109,14 @@ const Options = () => {
               <WhatsappIcon size={26} round />
             </WhatsappShareButton>
             <FacebookShareButton
-              url={`https://video-chat-mihir.web.app/`}
+              url={`https://naughty-shockley-2d2e6e.netlify.app/`}
               title={`Join this meeting with the given code :  "${me}"\n`}
               className={classes.share_icon}
             >
               <FacebookIcon size={26} round />
             </FacebookShareButton>
             <LinkedinShareButton
-              url={`https://video-chat-mihir.web.app/`}
+              url={`https://naughty-shockley-2d2e6e.netlify.app/`}
               title={`Join this meeting with the given code :  "${me}"\n`}
               className={classes.share_icon}
             >
