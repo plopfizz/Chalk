fbe65a71faa2353160765c677a71c90969bec354
Mohit Mandlecha <mohitmandlecha7256@gmail.com>
2021-05-17 23:19:09 +0530

    first commit

--- client/src/context/VideoState.js
+++ client/src/context/VideoState.js
@@ -3,7 +3,7 @@ import VideoContext from "./VideoContext";
 import { io } from "socket.io-client";
 import Peer from "simple-peer";
 
-const URL = "https://fathomless-tundra-67025.herokuapp.com/";
+const URL = "https://chalklive.herokuapp.com/";
 // const SERVER_URL = "http://localhost:5000/";
 
 export const socket = io(URL);
